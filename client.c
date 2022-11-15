87d65cbfbe60fbbcd671da8627f910469522d49b
Eduardo <eduher.rodp@gmail.com>
2022-11-14 20:28:46 -0600

    files

---
+++ client.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#define FIFO_FILE "MIFIFO"
+FILE *fp;
+struct reg{
+    int opc:
+    int ID;
+    int saldo;
+} registro;
+
+int main(int argc, char *argv[]){
+    
+}
